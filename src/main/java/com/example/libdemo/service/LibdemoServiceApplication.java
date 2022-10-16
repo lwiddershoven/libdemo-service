6d4036b75b0f4c46a9219c464bb9ec8759920472
lwiddershoven <lwiddershoven>
2022-10-16 15:18:27 +0200

    What most people do: copy from app to lib and do a component scan

--- src/main/java/com/example/libdemo/service/LibdemoServiceApplication.java
+++ src/main/java/com/example/libdemo/service/LibdemoServiceApplication.java
@@ -3,8 +3,17 @@ package com.example.libdemo.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.ComponentScan;
 
+//@SpringBootApplication(scanBasePackages = {
+//		"com.example.libdemo.service",
+//		"org.example.libdemo.lib.service"
+//})
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"com.example.libdemo.service",
+		"org.example.libdemo.lib.service"
+})
 @ConfigurationPropertiesScan
 public class LibdemoServiceApplication {
 
